46bc2072ed2ebd3a4ba5f153a29cf19e1674c107
Hawn <kouta.oishi455@gmail.com>
2021-01-25 03:29:31 +0900

    初回

---
+++ Cat/Assets/Scripts/Title/TitleController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class TitleController : MonoBehaviour
+{
+    [SerializeField]
+    AudioSource audioSource;
+
+    [SerializeField]
+    AudioClip audioClip1;
+
+    void Start()
+    {
+        StartCoroutine( CatVoice() );
+    }
+
+    IEnumerator CatVoice()
+    {
+        //while( true )
+        //{
+            yield return new WaitForSeconds( 3f );
+            audioSource.Play();
+        //}
+    }
+}
