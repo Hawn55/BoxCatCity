aee29c195fa1bf8fab92aab516c44c2bc454b4ef
Hawn <kouta.oishi455@gmail.com>
2021-01-30 03:05:21 +0900

    街モデル以外コミット

---
+++ Cat/Assets/Scripts/City/CityController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CityController : MonoBehaviour
+{
+    [SerializeField]
+    VolumeProfile m_VolumeProfile;
+
+    [SerializeField]
+    AudioSource m_VoiceAudioSource;
+
+    [SerializeField]
+    AudioClip m_CatAudio;
+
+    void Start()
+    {
+        StartCoroutine( CatVoice() );
+    }
+
+    IEnumerator CatVoice()
+    {
+        yield return new WaitForSeconds( 3f );
+        m_VoiceAudioSource.Play();
+    }
+}
