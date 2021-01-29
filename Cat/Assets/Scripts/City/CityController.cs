dd43b3323aef8769b3563186505cf9c0c07bf79a
Hawn <kouta.oishi455@gmail.com>
2021-01-30 03:42:06 +0900

    空回転設定

--- Cat/Assets/Scripts/City/CityController.cs
+++ Cat/Assets/Scripts/City/CityController.cs
@@ -1,12 +1,22 @@
 ﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
 
 public class CityController : MonoBehaviour
 {
+    [Header( "カメラアニメーション" )]
     [SerializeField]
-    VolumeProfile m_VolumeProfile;
+    Animator m_MainCameraAnimator;
 
+    [Header( "空環境" )]
+    [SerializeField] Volume m_SkyFogVolume;
+
+    HDRISky sky;
+    float sec;
+    float rot;
+
+    [Header( "声" )]
     [SerializeField]
     AudioSource m_VoiceAudioSource;
 
@@ -16,11 +26,22 @@ public class CityController : MonoBehaviour
     void Start()
     {
         StartCoroutine( CatVoice() );
+
+        m_SkyFogVolume.profile.TryGet( out sky );
+    }
+
+    void Update()
+    {
+        sec = Time.deltaTime;
+        sky.rotation.value += 1 * sec;
     }
 
     IEnumerator CatVoice()
     {
         yield return new WaitForSeconds( 3f );
         m_VoiceAudioSource.Play();
+
+        yield return new WaitForSeconds( 1f );
+        m_MainCameraAnimator.enabled = true;
     }
 }
